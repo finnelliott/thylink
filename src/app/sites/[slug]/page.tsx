0010c69043f27746a64f5f9444174015060d8556
finnelliott <finn@finnelliott.com>
2023-06-19 16:34:24 +0100

    --

--- src/app/sites/[slug]/page.tsx
+++ src/app/sites/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import prisma from "@/../../prisma/prismadb";
 import Site from "@/components/Site";
+import logo from "@/../public/thylink-logo-gray.png";
+import Image from "next/image";
 
 export default async function SiteHome({ params }: { params: { slug: string }}) {
     const user = await prisma.user.findUnique({
@@ -18,8 +20,13 @@ export default async function SiteHome({ params }: { params: { slug: string }}) 
         )
     }
     return (
+        <div className="w-screen min-h-screen relative">
         <main className="py-48 px-4 sm:px-6 lg:px-8">
             <Site user={user} />
         </main>
+        <footer className="w-screen flex items-center justify-center">
+            <a href="https://www.thyl.ink" target="_blank" rel="noreferer noopener" className="mx-auto flex justify-center absolute bottom-4 text-gray-300 items-center text-xs flex-row"><div className="mr-2">Made with</div><Image src={logo} width={113.78} height={32} className="text-gray-300 mt-1" alt="thyl.ink logo" /></a>
+        </footer>
+        </div>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import logo from "@/../public/thylink-logo-black.png";
 
 export default function Header() {
     return (
         <header className="flex justify-between items-center py-4">
             <div className="flex items-center justify-between max-w-7xl mx-auto w-full px-4 sm:px-6 lg:px-8">
-                <div>Links</div>
-                <div className="w-8 h-8"><UserButton /></div>
+                <div className="h-8 w-auto" ><Image src={logo} width={113.78} height={32} className="object-contain" alt="thyl.ink logo" /></div>
+                <div className="w-8 h-8 flex items-center justify-center"><UserButton /></div>
             </div>
         </header>
     )
